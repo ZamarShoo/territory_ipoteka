f8d1ad1a7ae6a0d672e63354ef316ce939036b51
ZamarShoo <shg.ivan97@gmail.com>
2022-01-20 21:21:15 +0300

    utp slide

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -97,5 +97,27 @@ const swiper = new Swiper('.garanty-body', {
       nextEl: '.garanty_navigate__next',
       prevEl: '.garanty_navigate__prev',
     },
-  });
+});
   
+const swiper_utp = new Swiper('.swiper-utp', {
+    // Optional parameters
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: false,
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        1200: {
+            slidesPerView: 3
+        },
+        1440: {
+            spaceBetween: 30
+        }
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.utp_navigate__next',
+      prevEl: '.utp_navigate__prev',
+    },
+});
