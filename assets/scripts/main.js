e0397a608531fa3bd5c02870a1e85fbf2dc3bcb8
ZamarShoo <shg.ivan97@gmail.com>
2022-01-24 21:03:40 +0300

    preloader

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -302,3 +302,38 @@ $('.close_map').click(function() {
         $('.map_popup__outer').hide()
     }, 400)
 })
+
+$(window).on('load', function () {
+    $('.preloader').delay(50).fadeOut('slow');
+});
+
+var tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 500,
+    loop: true
+  });
+  
+  // Add children
+  tl
+  .add({
+    targets: ['.loader .dot-1', '.loader .dot-2', '.loader .dot-3'],
+    opacity: 1,
+  })
+  .add({
+    targets: '.loader .dot-1',
+    translateX: 70,
+  })
+  .add({
+    targets: '.loader .dot-2',
+    translateX: 70,
+    delay: 200
+  })
+  .add({
+    targets: '.loader .dot-3',
+    translateX: 70,
+    delay: 200
+  })
+  .add({
+    targets: ['.loader .dot-1', '.loader .dot-2', '.loader .dot-3'],
+    opacity: 0,
+  });
